1241f0432075d0d605b1f37cc4b3f3b194c6a34a
Biraj Dahal <dahalbiraj@gmail.com>
2018-12-11 00:40:04 -0500

    added README

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ const { Client } = require('pg');
 const Discord = require('discord.js');
 const fs = require('fs');
 
-const token =  fs.readFileSync('./token.txt').toString().trim();
+const token = fs.readFileSync('./token.txt').toString().trim();
+const pass = fs.readFileSync('./password.txt').toString().trim();
 
 const db = new Client({
   user: 'postgres',
@@ -19,6 +20,26 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
+  if(msg.content == "reinitialize") {
+    // initialize database 
+  }
+  else if(msg.content.length > 5  && msg.content.substring(0,5) == "!win ") {
+    // add win
+
+    // extract player ids
+
+    /*let res = string.split(' ');
+    let playerIDs = []
+
+    for(let i = 1; i < res.length; i++) {
+        
+    }
+
+    f*/
+
+    console.log(msg.mentions.members.array());
+  }
+
   if(msg.content == "ping") {
     msg.reply("Pong!");
   }
